aa8efc67b5fb73b09cd2a74410c61f690fb11ac3
leemark <leemark@gmail.com>
2015-02-13 20:40:49 -0700

    scripting it up

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@
         go = document.getElementById('go'),
         day = document.getElementById('day'),
         month = document.getElementById('month'),
-        year = document.getElementById('year');
-    
-    
+        year = document.getElementById('year').options[e.selectedIndex].value;
     getInfo = function(){
         console.log(year);
     };
