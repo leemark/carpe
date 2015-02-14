2f2baf4e8e0662ba592fb70b93ac56923a913fc2
leemark <leemark@gmail.com>
2015-02-13 20:27:26 -0700

    scripting it up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('hahahhaha');
+(function(){
+    var getInfo,
+        go = document.getElementById('go'),
+        day = document.getElementById('day'),
+        month = document.getElementById('month'),
+        year = document.getElementById('year');
+    
+    go.addEventListener('click', getInfo, false);
+    getInfo = function(){
+        console.log(year);
+    };
+}());
