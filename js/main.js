8ba5ebc87ca65e56acb9ce8e2e255ae93b03bb2a
leemark <leemark@gmail.com>
2015-02-13 20:49:04 -0700

    scripting it up

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 (function(){
     var getInfo,
         go = document.getElementById('go'),
-        day = document.getElementById('day'),
-        month = document.getElementById('month'),
+        dayEl = document.getElementById('day'),
+        day = dayEl.options[dayEl.selectedIndex].value,
+        monthEl = document.getElementById('month'),
+        month = monthEl.options[monthEl.selectedIndex].value,
         yearEl = document.getElementById('year'),
-        year = yearEl.options[yearEl.selectedIndex].value;
+        year = yearEl.options[yearEl.selectedIndex].value,
+        genderEl = document.getElementById('gender'),
+        gender = genderEl.options[genderEl.selectedIndex].value;
     
     getInfo = function(){
-        console.log(year);
+        console.log(day + ':' + month + ':' + year + ':' + gender);
     };
     go.addEventListener('click', getInfo, false);
 }());
